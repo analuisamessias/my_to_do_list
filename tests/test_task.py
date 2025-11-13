7b762626fd5bb8348308a03248d1e6a3b52dba47
analuisamessias <analuisa.mendes1712@gmail.com>
2025-11-13 17:48:09 -0300

    update: update import TaskManager

--- tests/test_task.py
+++ tests/test_task.py
@@ -1,5 +1,9 @@
 import pytest
-from to_do_list.task_manager import TaskManager
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
+
+from to_do_list.task import TaskManager
 
 @pytest.fixture
 def manager():
